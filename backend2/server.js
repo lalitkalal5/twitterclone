a0eb7a0b407072e4e4f8f4d29075d56fcedaea54
Lalit kalal <lalitkalal171@gmail.com>
2024-09-16 12:35:35 +0530

    Update server.js

--- backend2/server.js
+++ backend2/server.js
@@ -10,7 +10,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from this origin
+    origin: "https://twitterclone-umber.vercel.app/", // Allow requests from this origin
     methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
     credentials: true, // Allow cookies and other credentials
   })
