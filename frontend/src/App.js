b5d4b38854d132d6abfe65b254a1f71e5621cf31
Lalit kalal <lalitkalal171@gmail.com>
2025-08-19 12:26:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,15 @@ import Profile from './components/Profile';
 import Edit from './components/Edit';
 import Search from './components/Search';
 import Userprofile from './components/Userprofile';
+import Login2 from './components/Login2';
+import RegistrationForm from './components/RegistrationForm';
 
 function App() {
   return (
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>} />
+       <Route path='/' element={<Login2/>} />
+       <Route path='/register' element={<RegistrationForm />} />
        <Route path='/feed' element={<Feed />} />
        <Route path='/profile' element={<Profile />} />
        <Route path='/userprofile/:userId' element={<Userprofile />} />
