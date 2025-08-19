1947e9e8eee8caba3bf8270b2f23e27641947d52
Lalit kalal <lalitkalal171@gmail.com>
2025-08-19 15:09:34 +0530

    Update tweet.model.js

--- backend2/models/tweet.model.js
+++ backend2/models/tweet.model.js
@@ -1,15 +1,18 @@
 import mongoose, {Schema} from "mongoose";
 
-const tweetSchema = new Schema({
-    content: {
-        type: String,
-        required: true
-    },
-    owner: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-}, {timestamps: true})
-
+// const tweetSchema = new Schema({
+//     content: {
+//         type: String,
+//         required: true
+//     },
+//     owner: {
+//         type: Schema.Types.ObjectId,
+//         ref: "User"
+//     }
+// }, {timestamps: true})
+const tweetSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true }
+}, { timestamps: true });
 
 export default mongoose.model('tweet',tweetSchema);
