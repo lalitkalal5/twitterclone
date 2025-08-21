695a0b18f43b4ae4afe68f32ec0c4d5b57fdfd8c
Lalit kalal <lalitkalal171@gmail.com>
2025-08-21 14:08:54 +0530

    Update tweet.model.js

--- backend2/models/tweet.model.js
+++ backend2/models/tweet.model.js
@@ -1,16 +1,27 @@
+// import mongoose from "mongoose";
+
+
+// const tweetSchema = new  mongoose.Schema({
+//     content: {
+//         type: String,
+//         required: true
+//     },
+//     owner: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "User"
+//     }
+// }, {timestamps: true})
+
+// export default mongoose.model("tweet", tweetSchema);
+
 import mongoose from "mongoose";
 
+const tweetSchema = new mongoose.Schema(
+  {
+    content: { type: String, required: true },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
+  },
+  { timestamps: true }
+);
 
-const tweetSchema = new  mongoose.Schema({
-    content: {
-        type: String,
-        required: true
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
-}, {timestamps: true})
-
-export default mongoose.model("tweet", tweetSchema);
-
+export default mongoose.model("tweet", tweetSchema); // capitalized name is conventional
